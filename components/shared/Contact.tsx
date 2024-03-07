5ec70301fb0e0fe6ea1c7b0209647d58aae1d6fb
PC <rahioui.youssef04@gmail.com>
2024-03-07 14:54:29 +0100

    fixed iframe

--- components/shared/Contact.tsx
+++ components/shared/Contact.tsx
@@ -30,7 +30,7 @@ const Contact = () => {
         </div>
 
     </section>
-    <iframe className="wrapper" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3325.3579829448536!2d-7.604817325209872!3d33.544074144393655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda632a31992cbbd%3A0x85012a06a160ffe!2s1191%20Av.%202%20Mars%2C%20Casablanca!5e0!3m2!1sfr!2sma!4v1709640228634!5m2!1sfr!2sma" width="100%" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe className="wrapper" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3325.3579829448536!2d-7.604817325209872!3d33.544074144393655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda632a31992cbbd%3A0x85012a06a160ffe!2s1191%20Av.%202%20Mars%2C%20Casablanca!5e0!3m2!1sfr!2sma!4v1709640228634!5m2!1sfr!2sma" width="100%" height="450"   loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
     </>
   )
 }
